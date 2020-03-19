342e39b925d9eacfb7a88b08b0b62450972240c5
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-03-19 10:23:40 +0800

    test git eclipse

--- src/main/java/com/psikku/backend/exception/DuplicateTestException.java
+++
@@ -1,8 +0,0 @@
-package com.psikku.backend.exception;
-
-public class DuplicateTestException extends RuntimeException {
-
-    public DuplicateTestException(String message) {
-        super(message);
-    }
-}
