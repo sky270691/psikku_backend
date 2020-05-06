c1027a9fad50f58774aa009b97ea627b445c786f
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-05-06 20:23:02 +0800

    start developing CovidResultTestCalculator class

---
+++ src/main/java/com/psikku/backend/service/uniquetestresultcalculator/CovidResultTestCalculator.java
@@ -0,0 +1,13 @@
+package com.psikku.backend.service.uniquetestresultcalculator;
+
+import com.psikku.backend.dto.test.SubmittedAnswerDto;
+
+import java.util.List;
+
+public class CovidResultTestCalculator implements UniqueResultTestCalculator{
+
+    @Override
+    public void calculateNewResult(List<SubmittedAnswerDto> submittedAnswerDtoList) {
+
+    }
+}
