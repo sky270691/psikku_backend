c0ce067b95cab7e95f78af5af66d68901026c1da
sky270691 <58143746+sky270691@users.noreply.github.com>
2020-05-06 21:30:46 +0800

    add @Service annotation in CovidResultTestCalculator class

--- src/main/java/com/psikku/backend/service/uniquetestresultcalculator/CovidResultTestCalculator.java
+++ src/main/java/com/psikku/backend/service/uniquetestresultcalculator/CovidResultTestCalculator.java
@@ -13,9 +13,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CovidResultTestCalculator implements UniqueResultTestCalculator{
 
     public final Logger logger = LoggerFactory.getLogger(CovidResultTestCalculator.class);
